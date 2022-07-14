ddf68f639dcd885c062dcddfb89e182e51332e0c
Егор Старшелюк <granegoro@gmail.com>
2022-07-14 17:46:21 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 100;
+        int bonus = (amount / 20);
+
+        int miles = bonus;
+
+        System.out.println(miles);
+    }
+}
